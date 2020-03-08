ebf57c09636e32b1fac73007f59761d7b517ef4d
Iain05 <58150364+Iain05@users.noreply.github.com>
2020-03-08 13:25:14 -0700

    Create Factorial-Function.java

---
+++ Factorial-Function.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Main {
+  	public static void main(String[] args) {
+		//   Scanner num = new Scanner(System.in);
+  	}
+	
+	public static long factorial(int num) {
+		if (num >= 1) {
+			 return num * factorial(num - 1);
+		} else {
+			return 1;
+		}
+	}
+}
