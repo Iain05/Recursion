7b75b15442af91881201755dd82daeab5e51fcc7
Iain05 <58150364+Iain05@users.noreply.github.com>
2020-03-08 13:43:49 -0700

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,3 +1,20 @@
 class Main {
-
+  	public static void main(String[] args) {
+    
+  	}	
+	int binarySearch(int[] arr, int l, int r, int x) {
+		{
+			if (r >= 1) {
+				int mid = l + (r - l) / 2;
+				if (arr[mid] == x) 
+					return mid;
+		
+				if (arr[mid] > x) 
+					return binarySearch(arr, l, mid - 1, x);
+				
+				return binarySearch(arr, mid + 1, r, x);
+			}
+			return -1;
+		}
+	}
 }
